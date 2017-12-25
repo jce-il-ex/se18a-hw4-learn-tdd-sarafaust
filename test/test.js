ac59b73c6c9883d2d6982a5ed269d5d239246a69
sarafaust <sarafaust1995@gmail.com>
2017-12-25 19:34:35 +0200

    RED: a failling test -should have a getChange Methud

--- test/test.js
+++ test/test.js
@@ -6,3 +6,14 @@ describe('Array', function(){
   })
 });
 var C = require('../cash.js');  // our module
+var assert = require("assert"); // core module
+var C = require('../cash.js');  // our module
+
+describe('Cash Register', function(){
+  describe('Module C', function(){
+    it('should have a getChange Method', function(){
+      assert.equal(typeof C, 'object');
+      assert.equal(typeof C.getChange, 'function');
+    })
+  })
+});
